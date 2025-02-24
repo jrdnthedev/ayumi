da375ac89f404d3e2f800826df5153e70ac94189
andre mcmorris <ajmcmorris@gmail.com>
2025-02-24 16:11:43 -0500

    added ui components and chat room page

---
+++ app/(pages)/chat-room/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ChatRoom() {
+    return (
+        <div>
+            <h1>Chat Room</h1>
+            <p>This is the chat room page</p>
+        </div>
+    )
+}
---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export function NavBar() {
+    return (
+        <>
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                    <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    </a>
+                    <div className="flex items-center space-x-6 rtl:space-x-reverse">
+                        <span className="text-sm  text-gray-500 dark:text-white hover:underline">Jon Jones</span>
+                        <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+                    </div>
+                </div>
+            </nav>
+            <nav className="bg-gray-50 dark:bg-gray-700">
+                <div className="max-w-screen-xl px-4 py-3 mx-auto">
+                    <div className="flex items-center">
+                        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                            <li>
+                                <Link href="/" className="text-gray-900 dark:text-white hover:underline">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/chat-room" className="text-gray-900 dark:text-white hover:underline">Chat Room</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
