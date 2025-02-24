7bbd6d367f0403b6692a777b5e41e551016937b1
andre mcmorris <ajmcmorris@gmail.com>
2025-02-24 17:01:11 -0500

    improved chat-room route to include nested routes and layout, added socket provider to create and share socket connection throught chat-room

---
+++ app/(pages)/chat-room/[roomId]/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Room() {
+    return (
+        <div>
+            <h1>Room</h1>
+            <p>This is the room page</p>
+        </div>
+    )
+}
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -8,7 +8,7 @@ export function NavBar() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ayumi</span>
                     </a>
                     <div className="flex items-center space-x-6 rtl:space-x-reverse">
                         <span className="text-sm  text-gray-500 dark:text-white">Jon Jones</span>
